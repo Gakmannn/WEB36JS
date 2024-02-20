04de161e16fe553d3fa9a071af8e43ab663cd389
Gakmannn <gnn87@mail.ru>
2024-02-20 20:11:07 +0300

    init vite

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import './style.css'
+console.log('Hello world')
+document.write('<div id="main" style="background: lightblue;">dfgdfgdf</div>')
+
+a = 25
+
+var a = 0
+console.log(a)
+var a = 2
+console.log(a)
+
+// cоздание переменной
+let newVar = 5
+newVar = 15
+
+let noVal
+noVal = 'g'
+console.log(noVal)
+
+let theValue
+
+// cоздание константы
+const DAY_IN_WEEK = 7
+// Ошибка не можем изменять константы
+// DAY_IN_WEEK = 8
+
+const countedSum = a + newVar
+
+console.log('10 + 5 =', 10+5)
+console.log('10 - 5 =', 10-5)
+console.log('10 * 5 =', 10*5)
+console.log('10 / 5 =', 10/5)
+console.log('10 ** 5 =', 10**5) //возведение в степень
+console.log('10 % 5 =', 10%5)   //остаток от деления
+console.log('3 % 2 =', 3%2)   //остаток от деления
+
+// let userInput = prompt('Введите число', 10)
+// console.log(userInput)
+// console.log(typeof userInput)
+// userInput = parseInt(userInput)
+// console.log(userInput)
+// console.log(typeof userInput)
+
+
+console.log('456')
+console.log(456)
+
+console.log((5-5%2)/2)
+
+// alert('Вы ввели ' + userInput)
+
+
+// Практика 1. Задание 1
+// Запросите у пользователя число, возведите это число во 2 - ю степень и выведите на экран.
+// let num = parseInt(prompt('Введите число',4))
+// alert ('Ваше число, возведённое во 2 степень' + num**2)
+
+// Практика 1. Задание 2
