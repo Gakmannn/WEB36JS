60df2821bdf0863b441a67a4acc7bf8ed9a24826
Gakmannn <gnn87@mail.ru>
2024-03-07 20:24:20 +0300

    рекурсия

--- src/main.ts
+++ src/main.ts
@@ -462,3 +462,108 @@ function getSumRest(...rest:number[]) {
 }
 
 console.log(getSumRest(1,2,43,55,546543,35,3))
+
+const string = 'ABC'
+for(let i=0; i<string.length; i++) {
+  console.log(string[i])
+}
+for(let char of string) {
+  console.log(char)
+}
+const array = [1,2,3]
+for(let i=0; i<array.length; i++) {
+  console.log(array[i])
+}
+for(let el of array) {
+  console.log(el)
+}
+const obj = {
+  a:1,
+  b:2,
+  c:3
+} as Record<string, number>
+for(let i=0; i<Object.keys(obj).length; i++) {
+  const key = Object.keys(obj)[i]
+  console.log(key, obj[key])
+}
+for(let key in obj) {
+  console.log(key, obj[key])
+}
+
+function powRecursive(x: number, n: number):number {
+  // return n == 1 ? x : x * powRecursive(x, n - 1)
+  if (n==1) {
+    console.log('Условие выхода стработало')
+    return x
+  } else {
+    console.log(n)
+    let result
+    console.log(result = x * powRecursive(x, n - 1))
+    return result
+  }
+}
+
+console.log(powRecursive(2,4))
+
+// function maxDethPowRecursive(x: number, n: number):number {
+//   console.log(n)
+//   return n == 1 ? x : x * maxDethPowRecursive(x, n - 1)
+// }
+
+// maxDethPowRecursive(1,100000)
+
+function powFor(x: bigint, n: number) {
+  let result = 1n;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+console.log(powFor(2n,100))
+
+// Написать функцию, которая выводит переданное ей число задом наперед.
+// Например: число 1234 вывести как 4321.
+
+function getReverseNumber(n: number):number {
+  if (n<10) {
+    return n
+  } else {
+    return +(n % 10 + '' + getReverseNumber(Math.floor(n/10)))
+  }
+}
+
+console.log(getReverseNumber(1234))
+
+// Написать функцию, которая считает сумму цифр числа.
+// Например: число 1357, сумма 1 + 3 + 5 + 7 = 16.
+
+function getSumOfDigits(n: number): number {
+  if (n < 10) {
+    return n
+  } else {
+    return n % 10 + getSumOfDigits(Math.floor(n / 10))
+  }
+}
+
+console.log(getSumOfDigits(1357))
+
+// Написать функцию, которая принимает число и выводит 
+// соответствующее количество вложенных пар круглых скобок.
+// Например: число 4 – (((())))
+
+
+function getBracketsPairsByNum(n: number): string {
+  if (!n) {
+    return '' 
+  } else {
+    return '(' + getBracketsPairsByNum(n-1) + ')'
+  }
+}
+
+// getBracketsPairsByNum(4) = '(' + getBracketsPairsByNum(3)== '((()))' + ')'     = (((())))
+// getBracketsPairsByNum(3) = '(' + getBracketsPairsByNum(2)== '(())' + ')'       = ((()))
+// getBracketsPairsByNum(2) = '(' + getBracketsPairsByNum(1)== '()' + ')'         = (())
+// getBracketsPairsByNum(1) = '(' + getBracketsPairsByNum(0)== '' + ')'           = ()
+
+console.log(getBracketsPairsByNum(4))
